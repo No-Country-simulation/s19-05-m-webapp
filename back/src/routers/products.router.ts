0bb0ec59260f8aff56b6c89d5e92490aeae21414
Marcos Soria <itsmarcos.1up@gmail.com>
2024-11-21 17:30:15 -0300

    Add: createProduct and updateProduct methods

--- back/src/routers/products.router.ts
+++ back/src/routers/products.router.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import { ProductController } from '../controllers/product.controller';
+import { ProductController } from "../controllers/product.controller";
 
 const productRouter = Router();
 const productController = new ProductController();
 
-productRouter.get('/', productController.getProductsWithLimitController);
+productRouter.get("/", productController.getProductsWithLimitController);
 productRouter.get("/:id", productController.getProductByIdController);
+
+productRouter.post(
+  "/create-product",
+  productController.createProductController
+);
+
+productRouter.put(
+  "/update-product/:id",
+  productController.updateProductController
+);
+
 export default productRouter;
--- back/src/services/product.service.ts
+++ back/src/services/product.service.ts
@@ -1,27 +1,59 @@
 import { productRepository } from "../repositories/product.repository";
+import { Product } from "../entity/Product.entity";
 
 export class ProductService {
-    async getProductsWithLimit(limit: number) {
-        try {
-            return await productRepository
-                .createQueryBuilder("products")
-                .take(limit) // Limita la cantidad de productos a devolver
-                .getMany();
-        } catch (error) {
-            console.error("Error fetching products:", error);
-            throw new Error("Failed to fetch products"); // El error será capturado en el controlador
-        }
+  async getProductsWithLimit(limit: number) {
+    try {
+      return await productRepository
+        .createQueryBuilder("products")
+        .take(limit) // Limita la cantidad de productos a devolver
+        .getMany();
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      throw new Error("Failed to fetch products"); // El error será capturado en el controlador
     }
+  }
 
-    async getProductById(id: number) {
-        try {
-            return await productRepository.findOne({
-                where: { id_product: id }, // Usa `idProduct` en lugar de `id`
-                relations: ["platforms"], // Incluye las plataformas relacionadas
-            });
-        } catch (error) {
-            console.error("Error in ProductService (getProductById):", error);
-            throw new Error("Failed to fetch product by ID");
-        }
+  async getProductById(id: number) {
+    try {
+      return await productRepository.findOne({
+        where: { id_product: id }, // Usa `idProduct` en lugar de `id`
+        relations: ["platforms"], // Incluye las plataformas relacionadas
+      });
+    } catch (error) {
+      console.error("Error in ProductService (getProductById):", error);
+      throw new Error("Failed to fetch product by ID");
     }
+  }
+
+  async createProduct(productData: Partial<Product>): Promise<Product> {
+    try {
+      const newProduct = productRepository.create(productData);
+      return await productRepository.save(newProduct);
+    } catch (error) {
+      console.error("Error creating product (createProduct):", error);
+      throw new Error("Failed to create product");
+    }
+  }
+
+  async updateProduct(
+    id: number,
+    productData: Partial<Product>
+  ): Promise<Product | null> {
+    try {
+      const product = await productRepository.findOne({
+        where: { id_product: id },
+      });
+
+      if (!product) {
+        return null;
+      }
+
+      productRepository.merge(product, productData); //merge es para combinar o actualizar (de typeorm)
+      return await productRepository.save(product);
+    } catch (error) {
+      console.error("Error updating product (updateProduct):", error);
+      throw new Error("Failed to update product");
+    }
+  }
 }
