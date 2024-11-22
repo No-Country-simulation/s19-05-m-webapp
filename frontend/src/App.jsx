3c95358d59cb85ca6df6e114228fcf1b5fc528bc
marcelYepes <marcelyepesqf@gmail.com>
2024-11-22 16:17:28 -0500

    configurando rutas en main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <div>E-Commerce</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 };
