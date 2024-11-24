21090c50079c02ff2cf39b2091f3131fa819b84d
marcelYepes <marcelyepesqf@gmail.com>
2024-11-24 16:24:44 -0500

    maquetacion inicial de header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <>
-      <div>E-Commerce</div>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,45 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header-container">
+      <div className="header-logo">
+        <Link to="/">
+          <img src="logo.png" alt="checkpoint logo" />
+        </Link>
+      </div>
+      <nav className="header-nav">
+        <ul className="header-list">
+          <div className="header-search">
+            <button className="header-search-btn">
+              <i class="bx bx-search-alt btn-search-p"></i>
+            </button>
+            <input
+              className="header-search-prod"
+              type="text"
+              placeholder="Minecraft, Pubg..."
+            />
+          </div>
+          <li className="header-item">
+            <Link to="/" className="header-item-link">
+              Inicio
+            </Link>
+          </li>
+          <li className="header-item">
+            <Link to="/" className="header-item-link">
+              Iniciar sesion
+            </Link>
+          </li>
+          <li className="header-item">
+            <Link to="/" className="header-item-link">
+              Productos
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
