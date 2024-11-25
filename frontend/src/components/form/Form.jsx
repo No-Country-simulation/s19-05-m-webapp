b77e758e3a42c553f23720688ed992d1995049e4
jfpanchi <jefpanchi@gmail.com>
2024-11-25 11:14:19 -0500

    feat: :sparkles: Add checkout form initial
    Add checkout route
    Add checkout page
    Add checkout form
    Add checkout form fields
    Update form
    - Add select

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -19,23 +19,40 @@ const Form = ({ fields, onSubmit, initialValues, buttonText }) => {
   
     return (
         <form onSubmit={handleSubmit}>
-            {
-                fields.map((field) => (
-                    <div key={field.name} className="form-group">
-                        <label htmlFor={field.name}>{field.label}</label>
-                        <input
-                            type={field.type || "text"}
-                            id={field.name}
-                            name={field.name}
-                            value={formValues[field.name] || ""}
-                            onChange={handleChange}
-                            placeholder={field.placeholder || ""}
-                        />
-                    </div>
-                ))
-            }
-            <button className="form-btn">{buttonText}</button>
-        </form>
+        {fields.map((field) => (
+            <div key={field.name} className="form-group">
+            <label htmlFor={field.name}>{field.label}</label>
+            {field.type === "select" ? (
+                <select
+                id={field.name}
+                name={field.name}
+                value={formValues[field.name] || ""}
+                onChange={handleChange}
+                >
+                {field.options.map((option, index) => (
+                    <option
+                    key={index}
+                    value={option.value}
+                    disabled={option.disabled || false}
+                    >
+                    {option.label}
+                    </option>
+                ))}
+                </select>
+            ) : (
+                <input
+                type={field.type || "text"}
+                id={field.name}
+                name={field.name}
+                value={formValues[field.name] || ""}
+                onChange={handleChange}
+                placeholder={field.placeholder || ""}
+                />
+            )}
+            </div>
+        ))}
+        <button className="form-btn">{buttonText}</button>
+    </form>
     );
 };
   
