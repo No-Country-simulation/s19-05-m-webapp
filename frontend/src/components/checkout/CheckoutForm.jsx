d422d53e85974b2dcf2689cb245e8baf2472d2d5
jfpanchi <jefpanchi@gmail.com>
2024-11-26 07:43:03 -0500

    feat: :sparkles: Add shipping state
    Add shipping slice
    Add shipping middleware
    Update store
    Update checkout form
    Update checkout styles

--- frontend/src/components/checkout/CheckoutForm.jsx
+++ frontend/src/components/checkout/CheckoutForm.jsx
@@ -1,20 +1,43 @@
+import { useDispatch, useSelector } from "react-redux";
 import checkoutFields from "../../utils/checkoutFields";
 import Form from "../form/Form";
 import "./checkout.css";
+import { setShipping } from "../../store/slices/shipping.slice";
+import { useState } from "react";
 
 export const CheckoutForm = () => {
+  const dispatch = useDispatch();
+  const shipping = useSelector((state) => state.Shipping);
+
+  const [rememberMe, setRememberMe] = useState(shipping?.name !== "");
+
   const handleCheckoutSubmit = (formValues) => {
-    console.log("Checkout:", formValues);
+    dispatch(setShipping(formValues));
+    if (rememberMe)
+      localStorage.setItem("shipping", JSON.stringify(formValues));
   };
 
   return (
     <div>
+      <h2>Formulario de envió</h2>
       <Form
         fields={checkoutFields.fields}
         onSubmit={handleCheckoutSubmit}
-        initialValues={checkoutFields.initialValues}
+        initialValues={shipping}
         buttonText="Enviar"
       />
+      <div className="remember-contain">
+        <label htmlFor="rememberMe">
+          {" "}
+          <input
+            type="checkbox"
+            id="rememberMe"
+            checked={rememberMe}
+            onChange={() => setRememberMe(!rememberMe)}
+          />
+          Recuérdame
+        </label>
+      </div>
     </div>
   );
 };
