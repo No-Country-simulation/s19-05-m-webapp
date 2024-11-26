fdc22b2fb17b60291c2d761ecec9f262cbf37212
ttaxyy <ttaxyy@proton.me>
2024-11-26 13:00:28 -0300

    Añadido diseño del login con OAuth

--- frontend/src/components/GoogleAuth/GoogleAuth.jsx
+++ frontend/src/components/GoogleAuth/GoogleAuth.jsx
@@ -1,7 +1,9 @@
 import { jwtDecode } from "jwt-decode";
+import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser, logout } from "../../store/slices/auth.slices";
+import "./GoogleAuth.css";
 
 const GoogleAuth = () => {
     const dispatch = useDispatch();
@@ -22,10 +24,10 @@ const GoogleAuth = () => {
             client_id: "152919958051-h2t2jisgn1hl97egdrst3uphhecsqp7s.apps.googleusercontent.com",
             callback: handleCallbackResponse,
         })
-
+            
         google.accounts.id.renderButton(
             document.getElementById("logInButton"),
-            { theme: "outline", size: "large" }
+            { theme: "filled_blue", size: "large" }
         )
         if (user) {
             document.getElementById("logInButton").style.display = "none";
@@ -35,17 +37,25 @@ const GoogleAuth = () => {
     }, [user]);
 
     return (
-        <div>
-            <div id="logInButton"></div>
-            {user && (
-                <>
-                    <button onClick={handleSignOut}>Sign Out</button>
-                    <div>
-                        <img src={user.picture} alt="User avatar" style={{width:100}}/>
-                        <h3>{user.name}</h3>
-                    </div>
-                </>
-            )}
+        <div id="logInDiv">
+            <div id="logIn-container">
+                <h1>Iniciar Sesión</h1>
+                <div id="logInButton"></div>
+                {user && (
+                    <>
+                        <button onClick={handleSignOut}>Sign Out</button>
+                        <div>
+                            <img src={user.picture} alt="User avatar" style={{width:100}}/>
+                            <h3>{user.name}</h3>
+                        </div>
+                    </>
+                 )}
+                <Link to="/registro" id="link-login">
+                ¿No estás registrado? Regístrate aquí.
+                </Link>
+                <div id="triangle-login"></div>
+            </div>
+            <div id="login-img"></div>
         </div>
     )
 }
