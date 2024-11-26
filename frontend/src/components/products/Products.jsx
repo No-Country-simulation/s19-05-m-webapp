30156ab22697b02fca802134901324071598ef9f
Maria Alarcon <mariagaa993@gmail.com>
2024-11-25 22:05:35 -0300

    seleccionar del dropdown y obtener el valor. Agregar el borrar filtros

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Card from "../card/Card";
 import Dropdown from "../dropdown/Dropdown";
@@ -8,17 +9,56 @@ import "./products.css";
 
 const Products = () => {
     const { data: products } = useFetch(productService.getProducts);
+    const [selectedOptions, setSelectedOptions] = useState({
+        console: '',  
+        category: '',     
+    });
     const navigate = useNavigate();
 
     const handleCard = (id) => {
         navigate(`/product/${id}`);
     };
 
+    const handleSelectChange = (e) => {
+        const { name, value } = e.target;
+        setSelectedOptions((prevState) => ({
+            ...prevState,
+            [name]: value,  
+        }));
+        console.log(`${name} seleccionada:`, value);  
+    };
+
+    const clearFilters = () => {
+        setSelectedOptions({
+            console: '',
+            category: '',
+        });
+    };
+
+    const isFilterActive = selectedOptions.console || selectedOptions.category;
+
     return (
         <>
             <div className="products-dropdown">
-                <Dropdown options={options.consoleOptions} />
-                <Dropdown options={options.categoryOptions}  />
+                <Dropdown 
+                    options={options.consoleOptions} 
+                    value={selectedOptions.console} 
+                    onChange={handleSelectChange} 
+                    name="console" 
+                />
+                <Dropdown 
+                    options={options.categoryOptions} 
+                    value={selectedOptions.category} 
+                    onChange={handleSelectChange} 
+                    name="category"
+                />
+                {
+                    isFilterActive && (
+                        <button className="clear-filters-btn" onClick={clearFilters}>
+                            Borrar filtros
+                        </button>
+                    )
+                }
             </div>
             <h1>Productos</h1>
             <div className="products-container">
