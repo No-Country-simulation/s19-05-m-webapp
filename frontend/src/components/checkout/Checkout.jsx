29ecdd10e836ce746d30a589aa5e67c0e0747507
jfpanchi <jefpanchi@gmail.com>
2024-11-27 22:05:05 -0500

    feat: :sparkles: Add payment checkout
    Add env template
    Add react paypal
    Update checkout
    - Add cart checkout component
    - Add payment checkout component

---
+++ frontend/src/components/checkout/Checkout.jsx
@@ -0,0 +1,20 @@
+export const Checkout = ({ onNext, products, totalAmount }) => {
+  return (
+    <div>
+      <h2>Cesta</h2>
+      {products.map((product, index) => (
+        <div key={index} className="summary-item">
+          <p>
+            {product.name} ({product.platform}) - {product.quantity}x{" "}
+            {product.price.toFixed(2)}$
+          </p>
+        </div>
+      ))}
+      <hr />
+      <p>
+        <strong>Total:</strong> {totalAmount.toFixed(2)}$
+      </p>
+      <button className="next-button" onClick={onNext}>Siguiente</button>
+    </div>
+  );
+};
--- frontend/src/components/checkout/CheckoutForm.jsx
+++ frontend/src/components/checkout/CheckoutForm.jsx
@@ -6,30 +6,10 @@ import { setShipping } from "../../store/slices/shipping.slice";
 import { useState } from "react";
 import { checkoutSchema } from "../../validations/checkout.schema";
 
-const products = [
-  {
-    name: "Minecraft: Java & Bedrock Edition",
-    quantity: 2,
-    price: 14.49,
-    platform: "Other",
-  },
-  { name: "7 Days to Die", quantity: 1, price: 17.18, platform: "Steam" },
-  {
-    name: "S.T.A.L.K.E.R. 2: Heart of Chornobyl",
-    quantity: 1,
-    price: 47.69,
-    platform: "Steam",
-  },
-];
-
-export const CheckoutForm = () => {
+export const CheckoutForm = ({ onGoBack, onFormSubmit }) => {
   const dispatch = useDispatch();
   const shipping = useSelector((state) => state.Shipping);
   const [errors, setErrors] = useState({});
-  const totalAmount = products.reduce(
-    (acc, product) => acc + product.price * product.quantity,
-    0
-  );
 
   const handleCheckoutSubmit = (formValues) => {
     checkoutSchema
@@ -38,6 +18,7 @@ export const CheckoutForm = () => {
         dispatch(setShipping(formValues));
         localStorage.setItem("shipping", JSON.stringify(formValues));
         setErrors({});
+        onFormSubmit();
         console.log({ formValues });
       })
       .catch((validationErrors) => {
@@ -57,42 +38,10 @@ export const CheckoutForm = () => {
         onSubmit={handleCheckoutSubmit}
         initialValues={shipping}
         errors={errors}
-        buttonText="Guardar"
-        showButton={false}
+        buttonText="Siguiente"
       />
 
-      {/* Sección Resumen */}
-      <h2>Resumen</h2>
-      <div className="summary-container">
-        {products.map((product, index) => (
-          <div key={index} className="summary-item">
-            <p>
-              {product.name} ({product.platform}) - {product.quantity}x{" "}
-              {product.price.toFixed(2)}$
-            </p>
-          </div>
-        ))}
-        <hr />
-        <p>
-          <strong>Total:</strong> {totalAmount.toFixed(2)}$
-        </p>
-      </div>
-
-      {/* Sección Total */}
-      <div className="total-container">
-        <p className="total-item">
-          <span>IVA (0%):</span> <span>0$</span>
-        </p>
-        <p className="total-item total-highlight">
-          <span>Total:</span> <span>{totalAmount.toFixed(2)}$</span>
-        </p>
-        <div className="pay-button">{/* TODO: Paypal button */}</div>
-        <p className="terms">
-          Haciendo clic en &quot;Pagar&quot; reconozco haber aceptado los{" "}
-          <a href="#terms">términos y condiciones</a>, y la{" "}
-          <a href="#privacy">política de privacidad</a>.
-        </p>
-      </div>
+      <button className="back-button" onClick={onGoBack}>Regresar</button>
     </div>
   );
 };
