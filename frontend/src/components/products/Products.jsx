a1e4bd11d94fd10b5602fff4ec1d38f4fe4a6578
Maria Alarcon <mariagaa993@gmail.com>
2024-12-04 07:46:06 -0300

    validar que el stock puede ser 0. Mostrar en la vista productos solo productos con stock disponible

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -10,7 +10,6 @@ import usePagination from "../../hooks/usePagination";
 import options from "../../utils/options";
 import useFilteredProducts from "../../hooks/useFilteredProducts";
 import "./products.css";
-import { useEffect } from "react";
 
 const Products = () => {
   const {
@@ -124,13 +123,15 @@ const Products = () => {
           <Loader />
         ) : filteredProducts && filteredProducts.length > 0 ? (
           filteredProducts.map((product) => (
-            <Card
-              key={product.id_product}
-              title={product.title}
-              genre={product.genre}
-              price={product.price}
-              onClick={() => handleCard(product.id_product)}
-            />
+            product.stock > 0 && (
+              <Card
+                key={product.id_product}
+                title={product.title}
+                genre={product.genre}
+                price={product.price}
+                onClick={() => handleCard(product.id_product)}
+              />
+            )
           ))
         ) : isFilterActive && filteredProducts.length === 0 ? (
           <p>
