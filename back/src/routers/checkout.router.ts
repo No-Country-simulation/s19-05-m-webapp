4324688c6266670dad68f2397343518dd70aa80e
Daniel Alejandro Romero <alejandro40000@hotmail.com>
2024-12-04 22:00:51 -0500

    Add document for checkouts router

--- back/src/routers/checkout.router.ts
+++ back/src/routers/checkout.router.ts
@@ -4,11 +4,78 @@ import { CheckoutController } from "../controllers/checkout.controller";
 const checkoutRouter = Router();
 const checkoutController = new CheckoutController();
 
+/**
+ * @swagger
+ * /checkouts/order:
+ *   post:
+ *     summary: Create a new order
+ *     description: Creates a new order in the checkout process.
+ *     responses:
+ *       201:
+ *         description: Order successfully created.
+ *       400:
+ *         description: Invalid input data.
+ */
 checkoutRouter.post("/order", checkoutController.createOrderController);
+
+/**
+ * @swagger
+ * /checkouts/capture:
+ *   get:
+ *     summary: Capture an order
+ *     description: Captures a payment for an order.
+ *     responses:
+ *       200:
+ *         description: Order captured successfully.
+ *       404:
+ *         description: Order not found.
+ */
 checkoutRouter.get("/capture", checkoutController.captureOrderController);
+
+/**
+ * @swagger
+ * /checkouts/cancel:
+ *   get:
+ *     summary: Cancel an order
+ *     description: Cancels an existing order.
+ *     responses:
+ *       200:
+ *         description: Order canceled successfully.
+ *       404:
+ *         description: Order not found.
+ */
 checkoutRouter.get("/cancel", checkoutController.cancelOrderController);
 
+/**
+ * @swagger
+ * /checkout:
+ *   get:
+ *     summary: Get all checkouts
+ *     description: Retrieves a list of all checkouts.
+ *     responses:
+ *       200:
+ *         description: A list of all checkouts.
+ */
 checkoutRouter.get("/", checkoutController.getAllCheckoutController);
+/**
+ * @swagger
+ * /checkout/{id}:
+ *   get:
+ *     summary: Get a checkout by ID
+ *     description: Retrieves a specific checkout by its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the checkout to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Checkout found.
+ *       404:
+ *         description: Checkout not found.
+ */
 checkoutRouter.get("/:id", checkoutController.getCheckoutByIdController);
 checkoutRouter.get("/status/:status", checkoutController.getCheckoutByIdController);
 
