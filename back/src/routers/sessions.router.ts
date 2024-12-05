84a6acadb446976918264e94be226ad77fe82fe6
Benjamín Peyraga <benjapey99@gmail.com>
2024-12-05 11:45:53 -0300

    Casi terminado el swagger de sesions

--- back/src/routers/sessions.router.ts
+++ back/src/routers/sessions.router.ts
@@ -1,16 +1,19 @@
 import { Router, Request, Response, NextFunction } from "express";
 import passport from "../middlewares/passportGoogle.mid";
 import { UserController } from "../controllers/users.controller";
-import { verifyTokenUtil } from "../utils/token.util";
-import { UserService } from "../services/users.service";
 
 const sessionRouter = Router();
-
-
-
 const userController = new UserController();
 
 /**
+ * @swagger
+ * tags:
+ *   name: Sessions
+ *   description: API for managing sessions, including register, login, online, signout sessions and Google oauth.
+ */
+
+/*
+*
  * @swagger
  * components:
  *   schemas:
@@ -74,12 +77,207 @@ const userController = new UserController();
  */
 
 // Registrar usuarios.
+/**
+ * @swagger
+ * /api/sessions/register:
+ *   post:
+ *     summary: Register a new user.
+ *     tags: [Sessions]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the user.
+ *               email:
+ *                 type: string
+ *                 description: The email of the user.
+ *               password:
+ *                 type: string
+ *                 description: The password for the user (hashed before storage).
+ *               address:
+ *                 type: string
+ *                 description: The address of the user.
+ *               phone:
+ *                 type: string
+ *                 description: The phone number of the user.
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *     responses:
+ *       201:
+ *         description: USER CREATED.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: USER CREATED.
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     token:
+ *                       type: string
+ *                       example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuZXdVc2VyIjp7Im5hbWUiOiJCZW5qYW1pbiBQZXlyYWdhIiwiZW1haWwiOiJiZW5qYXBleUBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRSMDVFV2JXeGxST0RDLlRZVVouSjJ1c3ZZMXVCRFEyUEhRbUcyN2J4dWZLUlMuRmY4UmRRQyIsImFjdGl2ZSI6dHJ1ZSwiYWRkcmVzcyI6IjQ1NiBFbG0gU3RyZWV0LCBNZXRyb3BvbGlzIiwicGhvbmUiOiIrMS01NTUtNjc4OSIsImlkX3VzZXJzIjo5LCJyb2xlIjoiVVNFUiJ9LCJpYXQiOjE3MzM0MDc2MjcsImV4cCI6MTczNDAxMjQyN30.6WoIVuqEjvu-iyqGgTdXw7KXi0T5VCjp13opH-oiMcE"
+ *                     user:
+ *                       type: object
+ *                       properties:
+ *                         name:
+ *                           type: string
+ *                           example: Juan Perez
+ *                         email:
+ *                           type: string
+ *                           example: JuanPerez@gmail.com
+ *                         password:
+ *                           type: string
+ *                           example: "$2b$10$R05EWbWxlRODC.TYUZ.J2usvY1uBDQ2PHQmG27bxufKRS.Ff8RdQC"
+ *                         active:
+ *                           type: boolean
+ *                           example: true
+ *                         address:
+ *                           type: string
+ *                           example: 456 Elm Street, Metropolis
+ *                         phone:
+ *                           type: string
+ *                           example: "+1-555-6789"
+ *                         id_users:
+ *                           type: integer
+ *                           example: 9
+ *                         role:
+ *                           type: string
+ *                           example: USER
+ *       400:
+ *         description: Bad request, invalid input data.
+ *       409:
+ *         description: Conflict, user with the provided email already exists.
+ *       500:
+ *         description: Internal server error.
+ */
+
 sessionRouter.post("/register", passport.authenticate("register", { session: false }), register);
 
 // Loguear usuarios.
+/**
+ * @swagger
+ * /api/sessions/login:
+ *   post:
+ *     summary: Authenticate a user and generate a session token.
+ *     tags: 
+ *       - Sessions
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The email of the user.
+ *                 example: user@example.com
+ *               password:
+ *                 type: string
+ *                 description: The user's password. This will be hashed before being compared with the stored hash.
+ *                 example: user_password_123
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       201:
+ *         description: Successfully logged in.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: USER LOGGED IN.
+ *                 token:
+ *                   type: string
+ *                   description: The JWT token to be used for further authenticated requests.
+ *                   example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE2ODMwMDAwMDB9.tYMCfBPWEXMxHPZi8U5VL23_HdYAH_vWXQGpdcwUMcA"
+ *                 user:
+ *                   type: object
+ *                   description: The user's information.
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                       example: John Doe
+ *                     email:
+ *                       type: string
+ *                       example: user@example.com
+ *                     active:
+ *                       type: boolean
+ *                       example: true
+ *                     address:
+ *                       type: string
+ *                       example: 123 Main Street, Springfield
+ *                     phone:
+ *                       type: string
+ *                       example: "+1-555-1234"
+ *                     id_users:
+ *                       type: integer
+ *                       example: 9
+ *                     role:
+ *                       type: string
+ *                       example: USER
+ *       400:
+ *         description: Bad request. The input data is invalid or incomplete.
+ *       401:
+ *         description: Unauthorized. Invalid email or password.
+ *       409:
+ *         description: Conflict. A user with the provided email already exists.
+ *       500:
+ *         description: Internal server error.
+ */
 sessionRouter.post("/login", passport.authenticate("login", { session: false }), login);
 
 // Consultar si está online.
+/**
+ * @swagger
+ * /api/sessions/online:
+ *   post:
+ *     summary: Validate a user session and set the user as online.
+ *     tags: 
+ *       - Sessions
+ *     parameters:
+ *       - in: header
+ *         name: token
+ *         required: true
+ *         description: JWT token to authenticate the user.
+ *         schema:
+ *           type: string
+ *           example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiVVNFUiIsInVzZXJfaWQiOjksImlhdCI6MTczMzQwOTQ4MCwiZXhwIjoxNzM0MDE0MjgwfQ.jrKcLB6-gLMswzT7XJ0YKuB1-o8T0UGr-h-Ut08clzw
+ *     responses:
+ *       201:
+ *         description: User successfully validated and marked as online.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: El usuario: benjapey@gmail.com is online
+ *                 token:
+ *                   type: string
+ *                   description: The same JWT token provided in the request.
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiVVNFUiIsInVzZXJfaWQiOjksImlhdCI6MTczMzQwOTQ4MCwiZXhwIjoxNzM0MDE0MjgwfQ.jrKcLB6-gLMswzT7XJ0YKuB1-o8T0UGr-h-Ut08clzw
+ *       400:
+ *         description: Bad request. Token is missing or invalid.
+ *       401:
+ *         description: Unauthorized. The token provided is invalid or expired.
+ *       500:
+ *         description: Internal server error.
+ */
 sessionRouter.post("/online", passport.authenticate("online", { session: false }), online);
 
 // Cerrar sesión de usuarios.
@@ -125,7 +323,7 @@ function online(req: Request, res: Response, next: NextFunction): void {
 // Funcion para signout un user.
 function signout(req: Request, res: Response, next: NextFunction): void {
     try {
-        const user = req.user ;
+        const user = req.user;
         res.status(200).json({ message: "USER SIGNOUT.", user });
     } catch (error) {
         next(error);
